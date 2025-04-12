0cde7653e22fee6c4fcb9b60545ba1a0f6d3692b
emmanuelnya <emmanuelnya123@gmail.com>
2025-04-13 01:23:23 +0200

    main program

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+#include "comptebancaire.h"
+#include "destinatario.h"
+
+int main() {
+    srand(time(0)); // Initialise le générateur de nombres aléatoires
+
+    comptebancaire compte;
+    compte.addTransaction(1500, "entrata", "2025-03-25", "stipendio");
+    compte.addTransaction(300, "uscita", "2025-03-30", "Andrea Fabrizio", "IT13K1523654780000001236547");
+    compte.addTransaction(1000, "entrata", "2025-04-01", "DEMIAN CANT");
+    compte.addTransaction(25, "uscita", "2025-04-02", "CONAD CITY-VIA DALMAZIA");
+    compte.affichercompte();
+
+    return 0;
+}
