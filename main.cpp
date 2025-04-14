0d4ba6cb39487fd7b0ac171788bf4222e970f6c4
emmanuelnya <emmanuelnya123@gmail.com>
2025-04-14 17:05:26 +0200

    transazioni bancari

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main() {
     compte.addTransaction(300, "uscita", "2025-03-30", "Andrea Fabrizio", "IT13K1523654780000001236547");
     compte.addTransaction(1000, "entrata", "2025-04-01", "DEMIAN CANT");
     compte.addTransaction(25, "uscita", "2025-04-02", "CONAD CITY-VIA DALMAZIA");
+    compte.addTransaction(0, "uscita","2025-01-31","SISAL");
     compte.affichercompte();
 
     return 0;
